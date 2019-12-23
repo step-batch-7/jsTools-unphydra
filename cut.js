ff9ff7faa07576b1fe99d2b08e0d18bac5529dca
rivu <58026024+unphydra@users.noreply.github.com>
2019-12-23 17:56:12 +0530

    performCut is modified, showOutput is added
    now output only show when there is an error or any content

--- cut.js
+++ cut.js
@@ -3,17 +3,12 @@ const { stdin } = process;
 const performCut = require("./src/performCut");
 
 const main = function() {
-  const printer = {
-    printContents: contents => {
-      console.log(contents);
-    },
-    printError: message => {
-      console.error(message);
-    }
+  const showOutput = function(cutOutput) {
+    cutOutput.cutLine && console.log(cutOutput.cutLine);
+    cutOutput.errorLine && console.error(cutOutput.errorLine);
   };
-
   const myFs = { fileReader: fs.readFile, fileExists: fs.existsSync };
-  performCut(myFs, process.argv, printer, stdin);
+  performCut(myFs, process.argv, showOutput, stdin);
 };
 
 main();
--- src/performCut.js
+++ src/performCut.js
@@ -1,39 +1,39 @@
 const { generateCutMessage, getCutLines, parser } = require("./optionLib");
 
-const performCutOnReadFile = function(error, data) {
-  const listOfLines = data.split("\n").slice(0, -1);
-  const listOfCutLines = getCutLines(listOfLines, this.parsedValue);
-  const result = generateCutMessage(listOfCutLines);
-  this.printer.printContents(result);
+const performCutOnReadFile = function(showOutput, parsedValue) {
+  return function(error, data) {
+    const listOfLines = data.split("\n").slice(0, -1);
+    const listOfCutLines = getCutLines(listOfLines, parsedValue);
+    const cutLine = generateCutMessage(listOfCutLines);
+    showOutput({ cutLine });
+  };
 };
 
-const performCutOnStdin = function(inst, stdin) {
+const performCutOnStdin = function(inst, showOutput, stdin) {
   stdin.setEncoding("utf8");
   stdin.on("data", data => {
     const listOfLines = data.split("\n").slice(0, -1);
-    const listOfCutLines = getCutLines(listOfLines, inst.parsedValue);
-    const result = generateCutMessage(listOfCutLines);
-    inst.printer.printContents(result);
+    const listOfCutLines = getCutLines(listOfLines, inst);
+    const cutLine = generateCutMessage(listOfCutLines);
+    showOutput({ cutLine });
   });
 };
 
-const performCut = function(fs, args, printer, stdin) {
+const performCut = function(fs, args, showOutput, stdin) {
   const parsedValue = parser(args);
-  const inst = { printer, parsedValue };
   if (!parsedValue.path) {
-    performCutOnStdin(inst, stdin);
+    performCutOnStdin(parsedValue, showOutput, stdin);
   } else {
     if (!fs.fileExists(parsedValue.path)) {
-      printer.printError(
-        `cut: ${parsedValue.path}: No such file or directory`
-      );
+      const errorLine = `cut: ${parsedValue.path}: No such file or directory`;
+      showOutput({ errorLine });
       return;
     }
-    fs.fileReader(
-      parsedValue.path,
-      "utf8",
-      performCutOnReadFile.bind(inst)
+    const performCutAfterRead = performCutOnReadFile(
+      showOutput,
+      parsedValue
     );
+    fs.fileReader(parsedValue.path, "utf8", performCutAfterRead);
   }
 };
 
