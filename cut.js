220dc4ec0657e81b3f0fe0daf5b46db4eac5ed55
rivu <58026024+unphydra@users.noreply.github.com>
2019-12-23 13:46:10 +0530

    v0.1 complete without validatiion of the args

--- cut.js
+++ cut.js
@@ -1,5 +1,22 @@
+const fs = require("fs");
+const {
+  generateCutMessage,
+  getCutLines,
+  getListOfFileContents,
+  parser
+} = require("./src/optionLib");
+
 const main = function() {
-  console.log("cut");
+  const myFs = { fileReader: fs.readFileSync };
+  const parsedValue = parser(process.argv);
+  const fileContents = getListOfFileContents(
+    myFs,
+    parsedValue.path,
+    "utf8"
+  );
+  const listOfCutLines = getCutLines(fileContents, parsedValue);
+  const result = generateCutMessage(listOfCutLines);
+  console.log(result);
 };
 
 main();
