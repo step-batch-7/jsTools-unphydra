2f7fc0190164ac8827b425927445e6ab3150c2da
rivu <58026024+unphydra@users.noreply.github.com>
2019-12-24 10:15:28 +0530

    stdin is modified to get input by using readline

--- cut.js
+++ cut.js
@@ -1,6 +1,12 @@
 const fs = require("fs");
-const { stdin } = process;
+// const { stdin } = process;
+const readline = require("readline");
 const performCut = require("./src/performCut");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 const main = function() {
   const showOutput = function(cutOutput) {
@@ -8,7 +14,7 @@ const main = function() {
     cutOutput.errorLine && console.error(cutOutput.errorLine);
   };
   const myFs = { fileReader: fs.readFile, fileExists: fs.existsSync };
-  performCut(myFs, process.argv, showOutput, stdin);
+  performCut(myFs, process.argv, showOutput, rl);
 };
 
 main();
--- src/performCut.js
+++ src/performCut.js
@@ -9,20 +9,21 @@ const performCutOnReadFile = function(showOutput, parsedValue) {
   };
 };
 
-const performCutOnStdin = function(inst, showOutput, stdin) {
-  stdin.setEncoding("utf8");
-  stdin.on("data", data => {
-    const listOfLines = data.split("\n").slice(0, -1);
+const performCutOnStdin = function(inst, showOutput, rl) {
+  rl.resume();
+  rl.on("line", line => {
+    const listOfLines = [line];
     const listOfCutLines = getCutLines(listOfLines, inst);
     const cutLine = generateCutMessage(listOfCutLines);
     showOutput({ cutLine });
   });
 };
 
-const performCut = function(fs, args, showOutput, stdin) {
+const performCut = function(fs, args, showOutput, rl) {
+  rl.pause();
   const parsedValue = parser(args);
   if (!parsedValue.path) {
-    performCutOnStdin(parsedValue, showOutput, stdin);
+    performCutOnStdin(parsedValue, showOutput, rl);
   } else {
     if (!fs.fileExists(parsedValue.path)) {
       const errorLine = `cut: ${parsedValue.path}: No such file or directory`;
