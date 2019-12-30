80ba852edb79d679ef430387bccd24862913f28f
rivu <58026024+unphydra@users.noreply.github.com>
2019-12-30 14:48:04 +0530

    testForSudo main is modified to test with sinon

--- test/testSudoMain.js
+++ test/testSudoMain.js
@@ -1,121 +1,85 @@
 const assert = require('chai').assert;
-const Events = require('events');
+const sinon = require('sinon');
 const { performCut } = require('../src/performCut');
 
 describe('performCut', () => {
   it('should call callback for readfile', () => {
-    const givenPath = 'somePath';
-    const givenEncoding = 'utf8';
     const argv = ['-d', ',', '-f', '1', 'somePath'];
-
-    const showOutput = function(cutOutput) {
-      assert.deepStrictEqual(cutOutput.cutLine, 'a\nb\nc');
-      assert.isUndefined(cutOutput.errorLine);
-    };
+    const showOutput = sinon.fake();
     const fs = {
-      readFile: function(path, encoding, callback) {
-        assert.deepStrictEqual(path, givenPath);
-        assert.deepStrictEqual(encoding, givenEncoding);
-        callback(null, 'a\nb\nc');
-      }
+      readFile: sinon.fake.yields(null, 'a,b\nb\nc')
     };
     performCut({fs}, argv, showOutput);
+    assert.ok(showOutput.calledWithExactly({cutLine: 'a\nb\nc'}));
   });
 
   it('should call callback for standerInput', () => {
-    const myEmitter = new Events();
     const argv = ['-d', ',', '-f', '1'];
-
-    const showOutput = function(cutOutput) {
-      assert.deepStrictEqual(cutOutput.cutLine, 'a');
-      assert.isUndefined(cutOutput.errorLine);
-    };
+    const myEmitter = {};
+    myEmitter.on = sinon.fake.yields('a,b');
+    const showOutput = sinon.fake();
     myEmitter.resume = () => {};
-
     performCut({fs: {}, readLine: myEmitter}, argv, showOutput );
-    myEmitter.emit('line', 'a,b');
+    assert.ok(showOutput.calledWithExactly({cutLine: 'a'}));
+    sinon.assert.calledOnce(showOutput);
+    sinon.assert.calledOnce(myEmitter.on);
   });
 
   it('should give no such file error if file is not present', () => {
-    const givenPath = 'somePath';
-    const givenEncoding = 'utf8';
     const argv = ['-d', ',', '-f', '1', 'somePath'];
-
-    const showOutput = function(cutOutput) {
-      assert.deepStrictEqual(
-        cutOutput.errorLine,
-        'cut: somePath: No such file or directory'
-      );
-      assert.isUndefined(cutOutput.cutLine);
-    };
+    const showOutput = sinon.fake();
     const possibleError = { code: 'ENOENT' };
     const fs = {
-      readFile: function(path, encoding, callback) {
-        assert.deepStrictEqual(path, givenPath);
-        assert.deepStrictEqual(encoding, givenEncoding);
-        callback(possibleError);
-      }
+      readFile: sinon.fake.yields(possibleError, null)
     };
     performCut({fs}, argv, showOutput);
+    const expected = {
+      errorLine: 'cut: somePath: No such file or directory',
+      exitCode: 1};
+    assert.ok(showOutput.calledWithExactly(expected));
+    sinon.assert.calledOnce(showOutput);
   });
 
   it('should give error reading error if a directory is given', () => {
-    const givenPath = 'somePathOfDir';
-    const givenEncoding = 'utf8';
     const argv = ['-d', ',', '-f', '1', 'somePathOfDir'];
-
-    const showOutput = function(cutOutput) {
-      assert.deepStrictEqual(
-        cutOutput.errorLine,
-        'cut: Error reading somePathOfDir'
-      );
-      assert.isUndefined(cutOutput.cutLine);
-    };
+    const showOutput = sinon.fake();
     const possibleError = { code: 'EISDIR' };
     const fs = {
-      readFile: function(path, encoding, callback) {
-        assert.deepStrictEqual(path, givenPath);
-        assert.deepStrictEqual(encoding, givenEncoding);
-        callback(possibleError);
-      }
+      readFile: sinon.fake.yields(possibleError, null)
     };
     performCut({fs}, argv, showOutput);
+    const expected = {
+      errorLine: 'cut: Error reading somePathOfDir',
+      exitCode: 74};
+    assert.ok(showOutput.calledWithExactly(expected));
+    sinon.assert.calledOnce(showOutput);
   });
 
   it('should permission denied if a unreadable file is given is given', () => {
-    const givenPath = 'unreadableFile';
-    const givenEncoding = 'utf8';
     const argv = ['-d', ',', '-f', '1', 'unreadableFile'];
-
-    const showOutput = function(cutOutput) {
-      assert.deepStrictEqual(
-        cutOutput.errorLine,
-        'cut: unreadableFile: Permission denied'
-      );
-      assert.isUndefined(cutOutput.cutLine);
-    };
+    const showOutput = sinon.fake();
     const possibleError = { code: 'EACCES' };
     const fs = {
-      readFile: function(path, encoding, callback) {
-        assert.deepStrictEqual(path, givenPath);
-        assert.deepStrictEqual(encoding, givenEncoding);
-        callback(possibleError);
-      }
+      readFile: sinon.fake.yields(possibleError, null)
     };
     performCut({fs}, argv, showOutput);
+    const expected = {
+      errorLine: 'cut: unreadableFile: Permission denied',
+      exitCode: 1};
+    assert.ok(showOutput.calledWithExactly(expected));
+    sinon.assert.calledOnce(showOutput);
   });
 
   it('should give usages if no option is given', () => {
     const argv = ['node', 'cut.js'];
-    const showOutput = function(cutOutput) {
-      assert.deepStrictEqual(
-        cutOutput.errorLine,
-        'usage: cut -b list [-n] [file ...]\n' +
-        '       cut -c list [file ...]\n' + 
-        '       cut -f list [-s] [-d delim] [file ...]'
-      );
-      assert.isUndefined(cutOutput.cutLine);
-    };
+    const showOutput = sinon.fake();
     performCut({}, argv, showOutput, {});
+    const expected = {
+      errorLine: 'usage: cut -b list [-n] [file ...]\n' +
+        '       cut -c list [file ...]\n' + 
+        '       cut -f list [-s] [-d delim] [file ...]',
+      exitCode: 1};
+    assert.ok(showOutput.calledWithExactly(expected));
+    sinon.assert.calledOnce(showOutput);
   });
 });
